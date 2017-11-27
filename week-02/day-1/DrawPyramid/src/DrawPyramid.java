83e388202d6b86c05bbc0fa775a7846cb95b4b76
Balint Bencz-Geher <balint.benczgeher@gmail.com>
2017-11-27 19:04:05 +0100

    Finished the dymind yeeeey

--- week-02/day-1/DrawPyramid/src/DrawPyramid.java
+++ week-02/day-1/DrawPyramid/src/DrawPyramid.java
@@ -1,13 +1,46 @@
 public class DrawPyramid {
     public static void main(String[] args) {
-        int num = 11;
-        int i = 0;
-        while (i < num + 1){
-            for (int j = 0; j < 2 * i + 1; j++ ){
-                System.out.print("*");
+        int num = 8;
+        int numHalf = num / 2;
+        if (num % 2 == 1) {
+            for (int i = 0; i < numHalf + 1; i++) {
+                for (int k = 0; k < numHalf - i; k++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
             }
-            System.out.println();
-            i++;
+            for (int i = 0; i < numHalf; i++) {
+                for (int j = 0; j < i + 1; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < 2 * numHalf - 1 - 2 * i; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        else{
+            for (int i = 0; i < numHalf; i++) {
+                for (int k = 0; k < numHalf - 1 - i; k++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < numHalf; i++){
+                for (int j = 0; j < i; j++ ) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k < 2 * numHalf - 1 - 2 * i; k++ ){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         }
     }
 }
