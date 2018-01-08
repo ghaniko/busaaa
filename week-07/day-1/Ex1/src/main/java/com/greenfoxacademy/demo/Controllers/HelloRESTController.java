f862364103f6160ebd14dde4e686ada53975128d
Balint Bencz-Geher <balint.benczgeher@gmail.com>
2018-01-08 16:16:05 +0100

    init(Greet Counter App): started and finished the exerces by implementing AtomicLing

--- week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloRESTController.java
+++ week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloRESTController.java
@@ -5,17 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 
 @RestController
 
-public class HelloRESTController {
 
+public class HelloRESTController {
+  AtomicLong myAtomic = new AtomicLong();
   @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-  public Greeting greeting(@RequestParam(value = "barmimas", required = false) String name){
+  public Greeting greeting(@RequestParam(value = "name", required = false) String name){
     if (name == null) {
       name = "world";
     }
-     Greeting myGreeting = new Greeting(1, name);
+    Greeting myGreeting = new Greeting(myAtomic.incrementAndGet(), name);
     return myGreeting;
   }
 }
