ad28f1f33b2f60732bfdc8f3b7b2a3773158aefc
Balint Bencz-Geher <balint.benczgeher@gmail.com>
2018-01-08 16:58:41 +0100

    init(Greet counter): added a counter variable to the previous exercise

--- week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloWebController.java
+++ week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloWebController.java
@@ -3,13 +3,19 @@ package com.greenfoxacademy.demo.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 
 public class HelloWebController {
+  AtomicLong myLong = new AtomicLong();
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World! (It is a web page.)");
+  public String greeting(Model model, @RequestParam(value = "name", required = false) String urlName) {
+
+    model.addAttribute("name", urlName);
+    model.addAttribute("count", myLong.incrementAndGet());
     return "greeting";
   }
 }
