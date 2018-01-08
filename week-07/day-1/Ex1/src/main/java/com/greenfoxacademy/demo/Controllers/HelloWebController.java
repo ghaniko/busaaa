59a0bee6931ae67caa046e14fb3a794861c5b4d7
Balint Bencz-Geher <balint.benczgeher@gmail.com>
2018-01-08 18:05:15 +0100

    finished last exercise implemented random size color and greeting

--- week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloWebController.java
+++ week-07/day-1/Ex1/src/main/java/com/greenfoxacademy/demo/Controllers/HelloWebController.java
@@ -10,12 +10,21 @@ import java.util.concurrent.atomic.AtomicLong;
 @Controller
 
 public class HelloWebController {
-  AtomicLong myLong = new AtomicLong();
+
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam(value = "name", required = false) String urlName) {
-
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    int randomNumber = (int) (Math.random() * hellos.length);
+    String[] colors = {"red", "green", "blue", "yellow", "pink", "purple"};
+    int randomNumber2 = (int) (Math.random() * colors.length);
+    int myRandomSize = (int) (Math.random() * 60) + 15;
+    model.addAttribute("mySize", myRandomSize );
+    model.addAttribute("myColor", colors[randomNumber2]);
+    model.addAttribute("oneHello", hellos[randomNumber]);
     model.addAttribute("name", urlName);
-    model.addAttribute("count", myLong.incrementAndGet());
     return "greeting";
   }
 }
