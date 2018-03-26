1fb8c93f37ccff09d8f3149bdc68537255e46ed1
Balint Bencz-Geher <balint.benczgeher@gmail.com>
2018-03-26 10:18:20 +0200

    feat(firstArrayUniqueElement method): handle duplicate elemnet

--- final-test/src/finalExamClass.java
+++ final-test/src/finalExamClass.java
@@ -1,23 +1,24 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class finalExamClass {
   public static void main(String[] args) {
-    List<Integer> firstArrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-    List<Integer> secondArrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    List<Integer> firstArrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 3, 4, 5, 6, 7, 8, 9));
+    List<Integer> secondArrayList = new ArrayList<>(Arrays.asList(1, 2, 2, 5, 6, 7, 8, 9, 10));
     System.out.println(firstArrayUniqueElement(firstArrayList, secondArrayList));
   }
 
   private static ArrayList<Integer> firstArrayUniqueElement(List<Integer> firstArrayList, List<Integer> secondArrayList) {
     ArrayList<Integer> resultArray = new ArrayList<>();
     for (int i = 0; i < firstArrayList.size(); i++) {
-      if (!secondArrayList.contains(firstArrayList.get(i))){
+      if (!secondArrayList.contains(firstArrayList.get(i)) && !resultArray.contains(firstArrayList.get(i))){
         resultArray.add(firstArrayList.get(i));
       }
     }
-    if (resultArray.isEmpty()) {
+    if (firstArrayList.isEmpty()) {
+      System.out.print("The first list is empty: ");
+    } else if (resultArray.isEmpty()){
       System.out.print("All elements of the first list are in the second, your array is: ");
     } else {
       System.out.print("Elements which are only in the first list: ");
